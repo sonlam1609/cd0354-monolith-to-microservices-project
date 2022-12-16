fd16dd960c0a89426921e352c967ccff544d454c
sonlam1609 <nguyenlamson1609@gmail.com>
2022-12-16 14:09:47 +0700

    update code

--- set_env.sh
+++ set_env.sh
@@ -1,11 +1,11 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
 export POSTGRES_USERNAME=postgres
-export POSTGRES_PASSWORD=mypassword
-export POSTGRES_HOST=postgres.cr9bldgsf1j6.us-east-1.rds.amazonaws.com
+export POSTGRES_PASSWORD=lamson1609
+export POSTGRES_HOST=db-udacity.csw9mo5sbpae.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::mybucket1200798
+export AWS_BUCKET=arn:aws:s3:::udacity-project3-735333745185
 export AWS_REGION=us-east-1
 export AWS_PROFILE=default
-export JWT_SECRET=testing
+export JWT_SECRET=FwoGZXIvYXdzEND//////////wEaDNcbI+rPx44bQaH08SLVAYa5rCZEnJUaZmBM02qIaqbKgNrzzHYGX0zS+FHKK4qhrA3JA6ftcnA/u5viQp4Q3U0vbX8YOx7GhyIGf1PaiZ1oLsoJck+MST6wSYS7VLirQpfDrOID1SLUuq3FqljQUssSKJTfB3Lo9742LnfIwq2zWx2EjFmT4pZO05p5mnEezr/hMDIGGaFPCGwgIsp/eeGP5Q0P/Mr2PKhsZfyHz4RTCwm+E+OzzlcrpOcp0XF9lTtHY2+8LmTvxw3+fGzilyHMTCiURgPNrFcVSRghvaYDULG4miiLiOucBjItAX/SJygB1rzIBYMhiHA76zQqC+jvsmm1xJ8B+zhs3nb9h11n5Z5yRHfOIewc
 export URL=http://localhost:8100
